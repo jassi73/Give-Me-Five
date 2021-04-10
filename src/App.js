da0c83f9879031e661209351c803ae7c73d42f22
JASA RAM <jessparihar73@gmail.com>
2021-04-10 22:11:07 +0530

    Give me Five project finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const chars = ["P", "L", "P", "T", "S", "A", "C", "E", "H", "D"];
+  const words = ["head", "apple", "space", "help", "place"];
+  const [givemestarted, setGivemestared] = useState(false);
+  const [enteredword, setEnteredword] = useState("");
+  const [correctwords, setCorrectwords] = useState([]);
+  function charSelect(char) {
+    if (enteredword.length > 4) {
+      setEnteredword("");
+      return;
+    }
+    setEnteredword(enteredword + char);
+  }
+  useEffect(() => {
+    if (words.includes(enteredword.toLowerCase())) {
+      let arr = correctwords;
+      arr.push(enteredword);
+      setCorrectwords(arr);
+      setEnteredword("");
+    }
+  }, [enteredword]);
+  console.log(enteredword);
+  console.log(correctwords);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Give Me Five Words</h1>
+      <button onClick={() => setGivemestared(true)}> Play </button>
+      {givemestarted ? (
+        <div class="  ">
+          <span>Entered Word{enteredword}</span>
+          <div>
+            {chars.map((char) => (
+              <span onClick={() => charSelect(char)} class="character">
+                {char}
+              </span>
+            ))}
+          </div>
+          {correctwords.map((item, id) => (
+            <div>
+              <input value={item} readOnly></input> <span>{item.length}</span>
+            </div>
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 }
